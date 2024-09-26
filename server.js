0e43c88b212aa4c22e8c2398aaf09f13ffa108d7
grace-kid <163421106+grace-kid@users.noreply.github.com>
2024-09-26 10:51:17 +0100

    Update server.js mysql

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use(cookieParser());
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST ,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD ,
-  database: process.env.DB_NAME ,
+  host: process.env.MYSQL_ADDON_HOST ,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD ,
+  database: process.env.MYSQL_ADDON_DB ,
 });
 
 db.connect(err => {
